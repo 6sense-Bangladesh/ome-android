50cf5819ddede80937186c2021177be6d919670f
Imamul Rana <imamulhasan@6sensehq.com>
2024-11-27 15:31:46 +0600

    Add new fragment_launch.xml, Update SupportFragment.kt
    -add new lines in strings.xml
    -add ome_logo_transparent.png
    -add new items in styles.xml

--- app/src/main/java/com/ome/app/presentation/dashboard/settings/support/SupportFragment.kt
+++ app/src/main/java/com/ome/app/presentation/dashboard/settings/support/SupportFragment.kt
@@ -2,6 +2,7 @@ package com.ome.app.presentation.dashboard.settings.support
 
 import android.content.Intent
 import android.net.Uri
+import android.widget.AdapterView
 import androidx.fragment.app.viewModels
 import com.ome.app.databinding.FragmentSupportBinding
 import com.ome.app.presentation.base.BaseFragment
@@ -14,42 +15,31 @@ class SupportFragment :
 
     override fun setupListener() {
         binding.topAppBar.setNavigationOnClickListener(::onBackPressed)
-//        binding.autoShutOffSelector.onItemClickListener =
-            /*AdapterView.OnItemClickListener { _, _, position, _ ->
-                viewModel.selectedTime = viewModel.timeList.getOrNull(position)?.second.orZero()
-            }*/
+        binding.autoShutOffSelector.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                viewModel.selectedTopic = viewModel.topicsArray[position]
+            }
 
-    }
-
-    override fun setupUI() {
-
-        binding.autoShutOffSelector.setSimpleItems(viewModel.topicsArray.toTypedArray())
-    }
-
-/*    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.autoShutOffSelector.setOnItemSelectedListener { view, position, id, item ->
-            viewModel.selectedTopic = viewModel.topicsArray[position]
-        }
-        binding.backIv.setOnClickListener {
-            findNavController().popBackStack()
-        }
         binding.sendBtn.setOnClickListener {
-            if (binding.textEt.text.toString()
+            if (binding.text.toString()
                     .isNotEmpty() && viewModel.selectedTopic.isNotEmpty()
             ) {
                 sendMessage()
             }
         }
-    }*/
+
+    }
+
+    override fun setupUI() {
+        binding.autoShutOffSelector.setSimpleItems(viewModel.topicsArray.toTypedArray())
+    }
 
     private fun sendMessage() =
         startActivity(Intent.createChooser(Intent(Intent.ACTION_SENDTO).apply {
             data = Uri.parse(
                 "mailto:" + Uri.encode(viewModel.supportEmail) +
-                        "?subject=" + Uri.encode(viewModel.selectedTopic) + ""
-//                        "&body=" + Uri.encode(binding.textEt.text.toString())
+                        "?subject=" + Uri.encode(viewModel.selectedTopic) +
+                        "&body=" + Uri.encode(binding.text.text?.toString())
             )
         }, "Send mail..."))
 
