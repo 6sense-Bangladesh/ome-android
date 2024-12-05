e23215c105bd55663346988e3e17909e031ccd2c
Ahmad Umar Mahdi <ahmadmahdi@6sensehq.com>
2024-12-05 17:48:49 +0600

    add NotificationManagerCompat to notify user

--- app/src/main/java/com/ome/app/presentation/PushNotificationService.kt
+++ app/src/main/java/com/ome/app/presentation/PushNotificationService.kt
@@ -1,11 +1,32 @@
 package com.ome.app.presentation
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.ome.app.R
+import kotlin.random.Random
 
 class PushNotificationService : FirebaseMessagingService() {
+
+    override fun onCreate() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager?.createNotificationChannel(notificationChannel)
+        }
+    }
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val title = remoteMessage.data["title"] ?: remoteMessage.notification?.title
+        val body = remoteMessage.data["body"] ?: remoteMessage.notification?.body
         // Handle FCM messages here
         Log.d(TAG, "From: ${remoteMessage.from}")
 
@@ -19,7 +40,19 @@ class PushNotificationService : FirebaseMessagingService() {
         remoteMessage.notification?.let {
             Log.d(TAG, "Message Notification Body: ${it.body}")
         }
+
+        val notificationBuilder =
+            NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                .setContentTitle(title.toString())
+                .setSmallIcon(R.drawable.ic_stove_fill)
+                .setContentText(body.toString())
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED)
+            NotificationManagerCompat.from(this).notify(randomId, notificationBuilder.build())
     }
+
+    private val randomId
+        get() = (System.nanoTime() + Random.nextInt(10000)).toInt()
 
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: $token")
@@ -28,5 +61,7 @@ class PushNotificationService : FirebaseMessagingService() {
 
     companion object {
         private const val TAG = "PushNotificationService"
+        private const val CHANNEL_ID = "ome_security_alerts"
+        private const val CHANNEL_NAME = "Ome Security Alerts"
     }
 }
