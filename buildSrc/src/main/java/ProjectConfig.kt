0d55cfbafa251bb3d3562501097a9fad2867bdb0
Ahmad Umar Mahdi <ahmadmahdi@6sensehq.com>
2024-12-10 13:47:05 +0600

    version up

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -7,7 +7,7 @@ object ProjectConfig {
     const val compileSdk = 35
     const val targetSdk = 35
 
-    const val versionCode = 25
+    const val versionCode = 27
     const val versionName = "1.0.033-ome-dev"
 
     val javaVersion = JavaVersion.VERSION_21
