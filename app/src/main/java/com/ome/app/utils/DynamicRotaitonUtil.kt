78378eaa464f4682b599ff6ae3dd33a2f4d9751c
Imamul Rana <imamulhasan@6sensehq.com>
2024-12-19 16:21:37 +0600

    Add DynamicRotaitonUtil.kt for device variable rotation.

---
+++ app/src/main/java/com/ome/app/utils/DynamicRotaitonUtil.kt
@@ -0,0 +1,21 @@
+package com.ome.app.utils
+
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import androidx.activity.ComponentActivity
+
+/**
+ * Checks the screen orientation & sets the orientation to portrait or landscape depending on the screen size
+ * @return true if the screen is a tablet, false otherwise
+ */
+fun ComponentActivity.dynamicRotation() {
+    val screenLayout = resources.configuration.screenLayout
+    val screenSize = screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK
+    val isTablet = screenSize >= Configuration.SCREENLAYOUT_SIZE_LARGE
+
+    requestedOrientation = if (isTablet) {
+        ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+    } else {
+        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+}
